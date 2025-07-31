1ebd5cca9050e0c69cbf5b910a10868cd61a26c0
JOOYEONGJUN <galn7071@naver.com>
2025-07-31 11:29:40 +0900

    Ajex Get연습

--- js/15_Ajax_Get_실습.js
+++ js/15_Ajax_Get_실습.js
@@ -4,6 +4,8 @@ $(function () {
   $("#btn1").click(문제1번기능);
   $("#btn2").click(userInfo); // ID가 btn1 를 클릭헸을때 문제1번 기능 함수에 담긴 기능 사용
   $("#btn3").click(getRandom);
+  $("#btn4").click(getComments);
+  $("#btn5").click(errorFn);
 });
 //문제 1 :  기본텍스트 데이터 가져오기
 // https://jsonplaceholder.typicode.com/posts/1
@@ -61,7 +63,6 @@ function userInfo() {
 }
 
 //문제 3 : 랜덤 명언 가져오기
-
 function getRandom() {
   // 1 . get 이용해서 데이터 가져올 주소 설정
   // https://api.quotable.io/random
@@ -69,17 +70,15 @@ function getRandom() {
   $.get("https://api.quotable.io/random")
 
     //데이터 가져오는데 문제가 없을 경우
-    .done( function(data){
-             $("#result3").html(
-     `<div class="success">
+    .done(function (data) {
+      $("#result3").html(
+        `<div class="success">
         <blockquote>${data.content}</blockquote>
              <strong>${data.author}</strong>
      <div>
-    `)
-        }
-   
-      )
-    
+    `
+      );
+    })
 
     .fail(
       //데이터 주소를 가져오는데 문제가 있는 경우
@@ -90,4 +89,66 @@ function getRandom() {
         `
       )
     );
+}
+
+// 문제 4  : 댓글 .length 개를 성공적으로 가져왔다 띄워주기
+// https://jsonplaceholder.typicode.com/posts/1/comments
+
+//문제 5 : 에러 처리하기
+//https://jsonplaceholder.typicode.com/posts/1/comments
+function getComments() {
+  $.get("https://jsonplaceholder.typicode.com/posts/1/comments")
+    .done(function (data) {
+      $("#result4").html(
+        `
+          <div class ="success">
+                    댓글 ${data.length} 개 를 성공적으로 불러왔습니다. 
+                    첫번째 댓글 ${data[0].body} index번호를 이용핫여 첫번째 댓글 가져오기
+                </div>                     
+                
+     `
+      );
+    })
+
+    .fail(function (data) {
+      $("#result4").html(
+        `<div class="success">
+                댓글을 가져오는데 실패 하였습니다.
+            <div>
+        `
+      );
+    });
+
+  //데이터 주소를 가져오는데 문제가 있는 경우
+}
+
+//문제 5 : 에러처리하기
+//https://jsonplaceholder.typicode.com/posts/999999
+function errorFn() {
+  $get("https://jsonplaceholder.typicode.com/posts/999999")
+    .done(function (data) {
+      $("#result5").html(
+        `
+            <div class ="success">
+                데이터를 성공적으로 가져 왔습니다. 
+            </div>
+            `
+      );
+    })
+
+    .fail(function (xhr) {
+      $("#result5").html(
+        `
+            <div class ="error">
+
+                애러 발생 !
+                <strong> 상태 코드 : </strong> ${xhr.status}<br>
+                <strong> 에러 메세지 : </strong> ${xhr.statusText}<br>
+
+
+            </div>
+            
+            `
+      );
+    });
 }
