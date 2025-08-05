8114b1644f9de9d8d3f0c5b2ccb612d0524c3a48
JOOYEONGJUN <galn7071@naver.com>
2025-08-05 17:55:13 +0900

    영화홈페이지실습

---
+++ 영화홈페이지실습/js/register.js
@@ -0,0 +1,8 @@
+function signup() {
+  hideMessages();
+
+  const username = $("#username").val().trim();
+  const email = $("#email").val().trim();
+  const password = $("#password").val();
+  const confirmPassword = $("#confirmPassword").val();
+}
