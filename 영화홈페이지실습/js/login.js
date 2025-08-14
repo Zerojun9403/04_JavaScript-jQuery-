3c161ec644d03e41077f30a02eb04acf915f5547
JOOYEONGJUN <galn7071@naver.com>
2025-08-14 11:56:10 +0900

    폴더삭제

--- 영화홈페이지실습/js/login.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $("#login").click();
-  $("#goToSignup").click(goToSignup);
-});
-
-function goToSignup() {
-  opener.location.href = "register.html";
-  window.close(); //현재로그인페이지 닫기
-}
